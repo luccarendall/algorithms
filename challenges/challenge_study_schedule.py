261d725b14bd23c1a8e202f7a824bfd3cfec1305
LuccaRendall <luccarendall2@gmail.com>
2023-03-28 07:40:01 -0300

    Feat: Função study_schedule completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,22 @@
 def study_schedule(permanence_period, target_time):
     # https://horadecodar.com.br/2020/12/08/if-com-multiplas-condicoes-em-python/
+    # https://bit.ly/3JUusaK
     pp = permanence_period
     tt = target_time
-    if pp is None or tt is None or type(pp) != int or type(tt) != int:
-        return None
+    num_students = 0
+    for initial_time, final_time in pp:
+        if pp is None or tt is None \
+                or type(initial_time) != int \
+                or type(final_time) != int:
+            return None
+        if initial_time <= target_time <= final_time:
+            num_students += 1
+            # Incrementa o contador se estudantes estiveram presentes
+            # durante o período de tempo
+    return num_students
 
-# 1.1 - Retorne a quantidade de estudantes presentes
-# para uma entrada específica;
-
-# 1.2 - Retorne None se em permanence_period houver alguma entrada inválida;
-
-# 1.3 - Retorne None se target_time recebe um valor vazio;
-
-# 1.4 - A função deverá, por meio de análise empírica, se comportar (no
-# avaliador remoto em sua Pull Request) como no máximo O(n), ou seja,
-# com complexidade assintótica linear.
+# Dica para o futuro:
+# Na validação de tipos tb poderia usar um isinstance(). Em havendo
+# necessidade de herança, seria uma escolha mais flexivel
+# Ex.: isinstance(initial_time, int)
+# https://bit.ly/3KdbGwB
