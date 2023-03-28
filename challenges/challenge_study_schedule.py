6ea082b62a758c93a1df86c016b575041fe13fa6
LuccaRendall <luccarendall2@gmail.com>
2023-03-28 06:59:50 -0300

    Feat: Função study_schedule parcial

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # https://horadecodar.com.br/2020/12/08/if-com-multiplas-condicoes-em-python/
+    pp = permanence_period
+    tt = target_time
+    if pp is None or tt is None or type(pp) != int or type(tt) != int:
+        return None
+
+# 1.1 - Retorne a quantidade de estudantes presentes
+# para uma entrada específica;
+
+# 1.2 - Retorne None se em permanence_period houver alguma entrada inválida;
+
+# 1.3 - Retorne None se target_time recebe um valor vazio;
+
+# 1.4 - A função deverá, por meio de análise empírica, se comportar (no
+# avaliador remoto em sua Pull Request) como no máximo O(n), ou seja,
+# com complexidade assintótica linear.
